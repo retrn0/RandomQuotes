6873158d5d4f975a86713824341450da8a61ce63
Kedar Joshi <kedarjo@gmail.com>
2018-05-28 18:03:06 -0700

    styles and updated functionality

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -8,6 +8,7 @@ class Quotes extends Component {
       quote: 'something very profound and inspirational'
     };
     this.getQuote = this.getQuote.bind(this);
+    this.tweetQuote = this.tweetQuote.bind(this);
   }
   getQuote() {
     console.log('sit tight, serving a fresh quote...');
@@ -27,12 +28,31 @@ class Quotes extends Component {
         });
       });
   }
+  componentDidMount() {
+    this.getQuote();
+  }
+  tweetQuote() {
+    console.log('tweet me do not console log me');
+  }
   render() {
     return (
-      <div>
-        <h1>{this.state.quote}</h1>
-        <h4> {this.state.author} </h4>
-        <button onClick={this.getQuote}> Quote Me </button>
+      <div className="wrapper">
+        <i className="fa fa-quote-left fa-3x quote"> </i>
+        <span className="quote"> {this.state.quote}</span>
+        <p className="author">
+          <i>{`--${this.state.author}`}</i>
+        </p>
+        <div className="footer">
+          <button className="btn-quote" onClick={this.getQuote}>
+            New Quote
+          </button>
+          <a href="#" onClick={this.tweetQuote}>
+            <i
+              className="fa fa-twitter-square fa-3x tweet-me"
+              aria-hidden="true"
+            />
+          </a>
+        </div>
       </div>
     );
   }
